0710e9934afdbfd784d770505f0626e806425cbd
Oshima jain <oshima.jain.al24@ggits.net>
2026-06-10 20:25:24 +0530

    Add median finding function for two sorted arrays
    Implement a solution for finding the median of two sorted arrays.

--- Arrays/004_Median_of_Two_Sorted_Arrays.py
+++ Arrays/004_Median_of_Two_Sorted_Arrays.py
@@ -1,1 +1,19 @@
+"""
+LeetCode #4
+Problem: Median of Two Sorted Arrays
+Difficulty: Hard
+"""
 
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
+        nums = nums1 + nums2
+
+        nums.sort()
+
+        n = len(nums)
+
+        if n % 2 == 1:
+            return float(nums[n // 2])
+
+        return (nums[n // 2 - 1] + nums[n // 2]) / 2
